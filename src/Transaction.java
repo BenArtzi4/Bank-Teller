149655e70955cda7e887209555cf880c07c1e885
BenArtzi4 <gal.ben.artzi.shachar@intel.com>
2022-11-20 01:24:47 +0200

    add pullTransaction method

--- src/Transaction.java
+++ src/Transaction.java
@@ -1,4 +1,19 @@
 public class Transaction
 {
+    private final int accountNumber;
+    private double amount;
 
+    public Transaction(int account, double amount)
+    {
+        this.accountNumber = account;
+        this.amount = amount;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
 }
--- src/TransactionsArray.java
+++ src/TransactionsArray.java
@@ -1,2 +1,27 @@
-public class TransactionsArray {
+import java.util.ArrayList;
+
+public class TransactionsArray
+{
+    ArrayList<Transaction> transactions;
+    private final static int FIRST = 0;
+    private final static int EMPTY = 0;
+
+
+    public TransactionsArray(Transaction [] transactions)
+    {
+        this.transactions = new ArrayList<Transaction>();
+    }
+
+    public Transaction pullTransaction()
+    {
+        if (transactions.size() == EMPTY)
+        {
+            return null;
+        }
+        int tempAccountNumber = transactions.get(FIRST).getAccountNumber();
+        double tempAmount = transactions.get(FIRST).getAmount();
+        Transaction tempTransaction = new Transaction(tempAccountNumber, tempAmount);
+        transactions.remove(FIRST);
+        return tempTransaction;
+    }
 }
