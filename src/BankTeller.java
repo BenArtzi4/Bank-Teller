d2c65da79ef541659a78ca0dee97ad2b5f2ba14b
BenArtzi4 <gal.ben.artzi.shachar@intel.com>
2022-11-20 13:05:21 +0200

    add print if transaction rejected

--- src/BankTeller.java
+++ src/BankTeller.java
@@ -1,7 +1,39 @@
+import java.util.ArrayList;
+
 public class BankTeller extends Thread
 {
+    private TransactionsArray transactions;
+
+    public BankTeller(TransactionsArray transactions)
+    {
+        this.transactions = transactions;
+    }
     @Override
-    public void run() {
+    public void run()
+    {
         super.run();
+
+        while (transactions.pullTransaction() != null)
+        {
+            Transaction currentTransaction = transactions.pullTransaction();
+            BankAccount currentBankAccount = currentTransaction.getAccount();
+
+        /*
+        If the operation cannot be executed as a result of the debtor entering a negative balance
+         */
+            if(!(currentBankAccount.trySetBalance(currentTransaction.getAmount())))
+            {
+                System.out.println("Transaction was rejected due to an attempt to enter a negative balance\n" +
+                        "Bank account: " + currentBankAccount.getAccountNumber() +
+                        "\n Current balance: " + currentBankAccount.getBalance() +
+                        "\nTransaction amount: " + currentTransaction.getAmount() +
+                        "\nBalance if the action was executed: " + (currentBankAccount.getBalance() +currentTransaction.getAmount())
+                );
+
+                transactions.addTransaction(currentTransaction);
+            }
+        }
+
+
     }
 }
