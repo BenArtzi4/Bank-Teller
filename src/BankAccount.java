146bfcf948a6fe18bfb933471d17c95cc97d5c22
BenArtzi4 <gal.ben.artzi.shachar@intel.com>
2022-11-20 12:05:41 +0200

    add BankTeller class

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,17 +1,13 @@
-public class BankAccount extends Thread
+public class BankAccount
 {
     private final int accountNumber;
     private double balance;
+    private final static int ZERO = 0;
 
     public BankAccount(int account, double amount)
     {
         this.accountNumber = account;
         this.balance = amount;
-    }
-
-    public void run()
-    {
-        super.run();
     }
 
     public double getBalance()
@@ -21,6 +17,14 @@ public class BankAccount extends Thread
 
     public void transaction(double amount)
     {
-        this.balance += amount;
+        // TODO: maybe using while the account is negative
+        if (amount > ZERO || this.balance - amount >= ZERO )
+        {
+            this.balance += amount;
+        }
+        else
+        {
+
+        }
     }
 }
---
+++ src/BankTeller.java
@@ -0,0 +1,7 @@
+public class BankTeller extends Thread
+{
+    @Override
+    public void run() {
+        super.run();
+    }
+}
