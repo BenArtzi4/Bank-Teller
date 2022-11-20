77e3d5c209213bf3ffb1b0ab18b4032c14e6104d
BenArtzi4 <gal.ben.artzi.shachar@intel.com>
2022-11-20 15:46:37 +0200

    add synchronized to pullTransiction method

--- src/TransactionsArray.java
+++ src/TransactionsArray.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class TransactionsArray
 {
@@ -6,27 +7,40 @@ public class TransactionsArray
     private final static int FIRST = 0;
     private final static int EMPTY = 0;
 
-
-    public TransactionsArray(Transaction [] transactions)
+    public TransactionsArray()
     {
         this.transactions = new ArrayList<Transaction>();
     }
 
-    public Transaction pullTransaction()
+    public TransactionsArray(Transaction [] transactions)
+    {
+        this.transactions = new ArrayList<Transaction>();
+        this.transactions.addAll(Arrays.asList(transactions));
+    }
+
+    public synchronized Transaction pullTransaction()
     {
         if (transactions.size() == EMPTY)
         {
             return null;
         }
-        BankAccount tempAccountNumber = transactions.get(FIRST).getAccount();
-        double tempAmount = transactions.get(FIRST).getAmount();
-        Transaction tempTransaction = new Transaction(tempAccountNumber, tempAmount);
+        Transaction currentTransaction = transactions.get(FIRST);
         transactions.remove(FIRST);
-        return tempTransaction;
+        return currentTransaction;
     }
 
     public void addTransaction(Transaction transaction)
     {
         this.transactions.add(transaction);
+    }
+
+    public Transaction getTransaction(int i)
+    {
+        return transactions.get(i);
+    }
+
+    public boolean isEmpty()
+    {
+        return this.transactions.size() == EMPTY;
     }
 }
